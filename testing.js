bed81045b3179820d7878a55df7be0f24a48f794
DayangAnis <dayanganis98@yahoo.com>
2025-07-13 22:38:44 +0800

    add new branch bro 	new file:   testing.js

---
+++ testing.js
@@ -0,0 +1,12 @@
+// text.js
+
+console.log("Hello, Dayang Anis! This is a test script.");
+
+// A simple function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Test the function
+const result = addNumbers(5, 7);
+console.log("The sum of 5 and 7 is:", result);
